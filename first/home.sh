bfe886fe2975fd77d47c96937066bb7956a2328b
Sergey <me@serge.xyz>
2018-03-14 12:27:02 +0300

    removeв extra variable

--- first/home.sh
+++ first/home.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-
 for var in 1 2 4
 do 
 
-omp_threads=$var
-export OMP_NUM_THREADS=$omp_threads
+export OMP_NUM_THREADS=$var
 
-echo "threads:" $omp_threads
+echo "threads:" $var
 echo "╔═══════════════════════════════════════════════════════╗"
 echo "║ dim   init            mul             runtime         ║"
 echo "╟───────────────────────────────────────────────────────╢"
