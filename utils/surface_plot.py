437624f7f260674401a72b443b8f8d1bea3a7e56
Sergey <a662833@ya.ru>
2018-05-23 15:26:16 +0300

    made some plots and began to draw up a report

--- utils/surface_plot.py
+++ utils/surface_plot.py
@@ -9,9 +9,11 @@ file = "surface"
 if len(sys.argv) > 1:
 	file = sys.argv[1]
 
-data = pd.read_csv("surface.csv", header=None, sep=" ")
+df = pd.read_csv("surface.csv", header=None, sep=" ")
 
-Z=np.array(data)
+details = df.tail(1)[0].values[0].split(",")
+
+Z=np.array(df[:-1].astype(float))
 
 X = np.arange(0, Z.shape[0], 1)
 Y = np.arange(0, Z.shape[1], 1)
@@ -22,5 +24,7 @@ fig = plt.figure()
 ax = Axes3D(fig)
 
 ax.plot_surface(X2D,Y2D, Z)
+
+plt.suptitle("threads: {}, iterations: {}, runtime: {}s, dim: {}, eps: {}".format(details[0], details[1], details[2], details[3], details[4]))
 plt.savefig("{}_plot.pdf".format(file))
 plt.show()
