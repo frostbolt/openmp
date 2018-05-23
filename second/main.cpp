381eaf623e4cf7fe8bf526ab0be351dee7217d3f
Sergey <a662833@ya.ru>
2018-05-23 14:51:22 +0300

    argv

--- second/main.cpp
+++ second/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <unistd.h>
+
 #include "functions.h"
 
-int main() {	
-	std::cout << solveDirichlet(100, 0.001).toString();
+int main(int argc, char *argv[]) {
+
+	char *opts = "dim:eps"; 
+
+	size_t n = 99;
+	double eps = 0.001;
+	int opt;
+	while((opt = getopt(argc, argv, opts)) != -1) {
+		switch(opt) {
+			case 'dim':
+				n = atoi(optarg);
+				break;
+			case 'eps':
+				eps = atoi(optarg);
+				break;
+		}
+	}
+
+	std::cout << solveDirichlet(n, eps).toString();
 	return 0;
 }
