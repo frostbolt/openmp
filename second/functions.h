132ef8d777b806bdeed6e9f64f8049ef6be8911e
Sergey <a662833@ya.ru>
2018-05-23 14:52:04 +0300

    output details

--- second/functions.h
+++ second/functions.h
@@ -9,18 +9,19 @@ struct DirichletResult {
 	Matrix surface;
 	size_t iterations;
 	double runtime;
+	double eps;
 
-	DirichletResult(size_t nt, Matrix s, size_t i, double r) : num_threads(nt), surface(s), iterations(i), runtime(r) {}
+	DirichletResult(size_t nt, Matrix s, size_t i, double r, double e) : num_threads(nt), surface(s), iterations(i), runtime(r), eps(e) {}
 
 	std::string toString() {
 		std::stringstream ss;
 
-		ss << surface.toString() << num_threads << "," << iterations << "," << runtime << std::endl;
+		ss << surface.toString() << num_threads << "," << iterations << "," << runtime << "," << surface.cols() << "," << eps << std::endl;
 		return ss.str();
 	}
 };
 
-Matrix solveDirichletSerial(size_t N=99, double eps=0.01);
-DirichletResult solveDirichlet(size_t N=99, double eps=0.01);
+Matrix solveDirichletSerial(size_t N=98, double eps=0.01);
+DirichletResult solveDirichlet(size_t N=98, double eps=0.01);
 
 #endif // __FUNCTIONS_H__
